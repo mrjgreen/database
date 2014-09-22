8a450d8891c2c29157ac1638a7a1d42237ebd008
Joe Green <joe.green0991@gmail.com>
2014-09-22 19:07:14 +0100

    insert ignore insert replace

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -1504,47 +1504,80 @@ class Builder {
 	 */
 	public function insert(array $values)
 	{
-		// Since every insert gets treated like a batch insert, we will make sure the
-		// bindings are structured in a way that is convenient for building these
-		// inserts statements by verifying the elements are actually an array.
-		if ( ! is_array(reset($values)))
-		{
-			$values = array($values);
-		}
-
-		// Since every insert gets treated like a batch insert, we will make sure the
-		// bindings are structured in a way that is convenient for building these
-		// inserts statements by verifying the elements are actually an array.
-		else
-		{
-			foreach ($values as $key => $value)
-			{
-				ksort($value); $values[$key] = $value;
-			}
-		}
-
-		// We'll treat every insert like a batch insert so we can easily insert each
-		// of the records into the database consistently. This will make it much
-		// easier on the grammars to just handle one type of record insertion.
-		$bindings = array();
-
-		foreach ($values as $record)
-		{
-			foreach ($record as $value)
-			{
-				$bindings[] = $value;
-			}
-		}
-
-		$sql = $this->grammar->compileInsert($this, $values);
-
-		// Once we have compiled the insert statement's SQL we can execute it on the
-		// connection and return a result as a boolean success indicator as that
-		// is the same type of result returned by the raw connection instance.
-		$bindings = $this->cleanBindings($bindings);
-
-		return $this->connection->insert($sql, $bindings);
+		return $this->doInsert($values, 'insert');
 	}
+
+    /**
+     * Insert a new record into the database.
+     *
+     * @param  array  $values
+     * @return bool
+     */
+    public function insertIgnore(array $values)
+    {
+        return $this->doInsert($values, 'insertIgnore');
+    }
+
+    /**
+     * Insert a new record into the database.
+     *
+     * @param  array  $values
+     * @return bool
+     */
+    public function replace(array $values)
+    {
+        return $this->doInsert($values, 'replace');
+    }
+
+    /**
+     * Insert a new record into the database.
+     *
+     * @param  array  $values
+     * @return bool
+     */
+    protected function doInsert(array $values, $type)
+    {
+        // Since every insert gets treated like a batch insert, we will make sure the
+        // bindings are structured in a way that is convenient for building these
+        // inserts statements by verifying the elements are actually an array.
+        if ( ! is_array(reset($values)))
+        {
+            $values = array($values);
+        }
+
+        // Since every insert gets treated like a batch insert, we will make sure the
+        // bindings are structured in a way that is convenient for building these
+        // inserts statements by verifying the elements are actually an array.
+        else
+        {
+            foreach ($values as $key => $value)
+            {
+                ksort($value); $values[$key] = $value;
+            }
+        }
+
+        // We'll treat every insert like a batch insert so we can easily insert each
+        // of the records into the database consistently. This will make it much
+        // easier on the grammars to just handle one type of record insertion.
+        $bindings = array();
+
+        foreach ($values as $record)
+        {
+            foreach ($record as $value)
+            {
+                $bindings[] = $value;
+            }
+        }
+
+        $sql = $this->grammar->{'compile' . ucfirst($type)}($this, $values);
+
+        // Once we have compiled the insert statement's SQL we can execute it on the
+        // connection and return a result as a boolean success indicator as that
+        // is the same type of result returned by the raw connection instance.
+        $bindings = $this->cleanBindings($bindings);
+
+        return $this->connection->insert($sql, $bindings);
+    }
 
 	/**
 	 * Insert a new record and get the value of the primary key.
--- src/Query/Grammars/Grammar.php
+++ src/Query/Grammars/Grammar.php
@@ -741,6 +741,39 @@ class Grammar {
 		return $joiner.$union['query']->toSql();
 	}
 
+    /**
+     * Compile an insert statement into SQL.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  array  $values
+     * @return string
+     */
+    protected function doCompileInsert(Builder $query, array $values, $type)
+    {
+        // Essentially we will force every insert to be treated as a batch insert which
+        // simply makes creating the SQL easier for us since we can utilize the same
+        // basic routine regardless of an amount of records given to us to insert.
+        $table = $this->wrapTable($query->from);
+
+        if ( ! is_array(reset($values)))
+        {
+            $values = array($values);
+        }
+
+        $columns = $this->columnize(array_keys(reset($values)));
+
+        // We need to build a list of parameter place-holders of values that are bound
+        // to the query. Each insert should have the exact same amount of parameter
+        // bindings so we can just go off the first list of values in this array.
+        $parameters = $this->parameterize(reset($values));
+
+        $value = array_fill(0, count($values), "($parameters)");
+
+        $parameters = implode(', ', $value);
+
+        return "$type into $table ($columns) values $parameters";
+    }
+
 	/**
 	 * Compile an insert statement into SQL.
 	 *
@@ -750,29 +783,32 @@ class Grammar {
 	 */
 	public function compileInsert(Builder $query, array $values)
 	{
-		// Essentially we will force every insert to be treated as a batch insert which
-		// simply makes creating the SQL easier for us since we can utilize the same
-		// basic routine regardless of an amount of records given to us to insert.
-		$table = $this->wrapTable($query->from);
-
-		if ( ! is_array(reset($values)))
-		{
-			$values = array($values);
-		}
-
-		$columns = $this->columnize(array_keys(reset($values)));
-
-		// We need to build a list of parameter place-holders of values that are bound
-		// to the query. Each insert should have the exact same amount of parameter
-		// bindings so we can just go off the first list of values in this array.
-		$parameters = $this->parameterize(reset($values));
-
-		$value = array_fill(0, count($values), "($parameters)");
-
-		$parameters = implode(', ', $value);
-
-		return "insert into $table ($columns) values $parameters";
+		return $this->doCompileInsert($query, $values, 'insert');
 	}
+
+    /**
+     * Compile an insert statement into SQL.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  array  $values
+     * @return string
+     */
+    public function compileInsertIgnore(Builder $query, array $values)
+    {
+        return $this->doCompileInsert($query, $values, 'insert ignore');
+    }
+
+    /**
+     * Compile an insert statement into SQL.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  array  $values
+     * @return string
+     */
+    public function compileReplace(Builder $query, array $values)
+    {
+        return $this->doCompileInsert($query, $values, 'replace');
+    }
 
 	/**
 	 * Compile an insert and get ID statement into SQL.
