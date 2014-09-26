f5c4ceb6bb1e8989e39a431aae1b1b530248acd9
Joe Green <joe.green0991@gmail.com>
2014-09-26 16:51:10 +0100

    connection resolver tests

--- tests/unit/Database/DatabaseConnectionResolverTest.php
+++ tests/unit/Database/DatabaseConnectionResolverTest.php
@@ -16,15 +16,27 @@ class DatabaseConnectionResolverTest extends PHPUnit_Framework_TestCase {
             'test1' => array(
                 'foo' => 'bar'
             ),
-            'test2' => array(
-                'baz' => 'boo'
-            )
+            'test2' => m::mock('Illuminate\Database\Connection')
         );
 
         $factory = m::mock('Illuminate\Database\Connectors\ConnectionFactory');
 
+        $connectionMock = m::mock('stdClass');
+
+        $factory->shouldReceive('make')->once()->with($configs['test1'])->andReturn($connectionMock);
+
 		$resolver = $this->getMock('Illuminate\Database\ConnectionResolver', null, array($configs, $factory));
 
 		$this->assertTrue($resolver->hasConnection('test1'));
+
+        $connection = $resolver->connection('test1');
+
+        $this->assertSame($connectionMock, $connection);
+
+        $factory->shouldReceive('make')->never();
+
+        $connection = $resolver->connection('test2');
+
+        $this->assertSame($configs['test2'], $connection);
 	}
 }
