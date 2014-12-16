9468d66a78951cc9232f252e9d92682a8d3a410e
Joe Green <joe.green0991@gmail.com>
2014-12-16 09:53:38 +0000

    Update config.php

--- tests/integration/config.php
+++ tests/integration/config.php
@@ -3,7 +3,7 @@ return array(
     'host'      => 'localhost',
     'driver'    => 'mysql',
     'username'  => 'root',
-    'password'  => 'password',
+    'password'  => '',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
