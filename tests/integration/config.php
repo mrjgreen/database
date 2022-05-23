f02b15602ca439b293be16943fb4b4c3b24d7cdb
Joe Green <joe.green@anaplan.com>
2022-05-23 08:07:23 +0100

    run actions

--- tests/integration/config.php
+++ tests/integration/config.php
@@ -12,5 +12,18 @@ return array(
         'options'   => array(
             PDO::ATTR_EMULATE_PREPARES => false
         )
+    ),
+    array(
+        'host'      => '127.0.0.1',
+        'port'      => 3306,
+        'driver'    => 'mysql',
+        'username'  => 'root',
+        'password'  => 'password',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+        'options'   => array(
+            PDO::ATTR_EMULATE_PREPARES => false
+        )
     )
 );
